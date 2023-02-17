b6160a4e792b162450811c3a8915ddd80ccab60d
JuanMaCarrearas <manuucarreras@gmail.com>
2023-02-17 20:17:19 -0300

    Nav

---
+++ src/Components/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function NavBar() {
+    return (
+        <>
+            <div className='container'>
+
+                <ul>
+
+
+                </ul>
+
+            </div>
+        </>
+    )
+}
+
+export default NavBar
