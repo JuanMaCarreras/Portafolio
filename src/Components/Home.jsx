b488ede8e351a7615fc1edaf3d528900b0377360
JuanMaCarrearas <manuucarreras@gmail.com>
2023-02-17 21:33:20 -0300

    home

---
+++ src/Components/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div className='container'>
+            <div>
+                <h1>Manuel Carreras</h1>
+
+                <h3>Full Stack Developer</h3>
+
+                <p>Hola!  </p>
+            </div>
+        </div>
+    )
+}
+
+export default Home
