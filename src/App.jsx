b878df3163d35d7a36253d97efeb304e653e8972
JuanMaCarrearas <manuucarreras@gmail.com>
2023-07-19 22:03:45 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App () {
+  return (
+    <>
+
+      <h1 className='text-red-700'>Hola Mundo!!!!!</h1>
+      <h2>Hola H2</h2>
+      <h3>Hola H3</h3>
+      <h4>Hola H4</h4>
+
+    </>
+  )
+}
+
+export default App
