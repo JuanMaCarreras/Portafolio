e7ebe70e30b5a59ba6210ba603c2a57f74966b66
JuanMaCarrearas <manuucarreras@gmail.com>
2023-07-21 18:21:47 -0300

    Tailwind Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: '#0d1a26',
+        mainDark: '#09121c',
+        second: '#d45498'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
