f4b7dcb5c254319b65beca4656b2a3c3592ea5a9
JuanMaCarrearas <manuucarreras@gmail.com>
2023-07-26 18:49:47 -0300

    config de colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,23 @@ export default {
     extend: {
       colors: {
         main: '#0d1a26',
-        mainDark: '#09121c',
-        second: '#d45498'
+        mainDark: '#0a1622',
+        mainSecond: '#011627',
+        second: '#d45498',
+        H: '#f06529',
+        C: '#2965f1',
+        J: '#f0db4f',
+        R: '#61dbfb',
+        RD: '#764abc',
+        T: '#4dc0b5',
+        N: '#58ab51',
+        E: '#b3afa9',
+        S: '#3490dc',
+        P: '#004c99',
+        MY: '#f29111',
+        G: '#f1502f',
+        GH: '#c7d1db', // '#5e5d5d',
+        I: '#522e92'
       }
     }
   },
