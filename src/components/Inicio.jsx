f0e560ece59f7e0fd9114bb9f15bacdd2b5ffde9
JuanMaCarrearas <manuucarreras@gmail.com>
2023-07-27 23:20:03 -0300

    text color change

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,11 +1,11 @@
 function Inicio () {
   return (
     <>
-      <main className='pl-44 py-20 text-white'>
-        <h1 className='text-6xl font-bold'>Manuel Carreras</h1>
+      <main className='pl-40 pt-32'>
+        <h1 className='text-6xl font-bold text-white'>Manuel Carreras</h1>
         <h2 className='text-2xl font-semibold text-second pt-2 pl-2'>Full Stack Developer</h2>
 
-        <p className='py-6 whitespace-pre-wrap mr-96 text-base pl-2'>¡Hola! Soy un creador digital obsesionado con la magia de la programación Full Stack.
+        <p className='py-6 whitespace-pre-wrap mr-96 text-base text-text pl-2'>¡Hola! Soy un entusiasta del mundo digital fascinado por la magia que se encuentra detrás de la programación Full Stack.
           Me encanta fusionar arte y funcionalidad para dar vida a proyectos únicos.
           Estoy en la busca de mi primera experiencia laboral, estoy ansioso por unirme a un equipo creativo y dejar una huella digital en el mundo.
           Siempre estoy abierto a nuevos desafíos y dispuesto a aprender. <br />
