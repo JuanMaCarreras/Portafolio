747b1d284c8fec9c52b5790eee51e2c7a0b61754
JuanMaCarrearas <manuucarreras@gmail.com>
2023-07-27 23:21:39 -0300

    img add

--- src/assets/infoPro.js
+++ src/assets/infoPro.js
@@ -1,9 +1,12 @@
+import imgC from './img/PI_2.png'
+import imgB from './img/books.png'
+
 const infoPro = [
   {
     id: 1,
     titulo: 'PI Countires (Proyecto Individual)',
     info: 'El objetivo de este proyecto es hacer una App usando React, Redux, NodeJS, Sequelize y Express. - Desarrollando y gestionando servidores con (Express y NodeJs) Base de Datos con ( Postgres SQL y Sequelize) - Desarrollando y gestionando la parte del cliente con (ReactJs, Redux, HTML y CSS) React y sus estados locales / coompnentes y sus siclos de vida Redux y sus estados globales',
-    img: '',
+    img: imgC,
     repo: 'https://github.com/JuanMaCarreras/PI-Countries'
 
   },
@@ -11,7 +14,7 @@ const infoPro = [
     id: 2,
     titulo: 'Henry Books',
     info: 'Diseñar y desarrollar un E-Commerce de Libros que incluía: búsquedas, filtrados, ordenamientos, creación, autenticaciones de usuarios, pasarela de pagos, chatbot, reviews, dashboard, productos destacados. Trabaje en equipo mediante la metodología ágil SCRUM, GitHub WorkFlow, utilizando ramas, creando pull request y controlando las versiones de desarrollo y producción para desarrollar un producto digital',
-    img: './',
+    img: imgB,
     repo: 'https://github.com/scch94/probando-pf'
 
   }
