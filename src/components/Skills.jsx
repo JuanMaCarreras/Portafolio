2e7d23b864db90202f9bb32721373539ad646ca9
JuanMaCarrearas <manuucarreras@gmail.com>
2023-07-31 21:15:54 -0300

    Skills Done

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,10 +5,10 @@ function Skills () {
   return (
     <>
       <article id='skills'>
-        <h3 className='text-3xl font-semibold text-second pl-44 pt-32'>Habilidades</h3>
+        <h3 className='text-3xl font-semibold text-second pl-36 pt-32'>Habilidades</h3>
 
-        <h1 className='flex text-white text-2xl justify-start mt-20 ml-48 mr-96 border-b'>Front-End</h1>
-        <div className='flex justify-start mb-4 ml-48 mr-96 gap-6 py-8 px-5 border-t-8 border-mainDark '>
+        <h1 className='flex text-white text-2xl justify-start mt-20 ml-44 mr-96 border-b'>Front-End</h1>
+        <div className='flex justify-start mb-4 ml-44 mr-96 gap-6 py-8 px-5 border-t-8 border-mainDark '>
 
           <DiHtml5 className='text-text h-14 w-14 hover:duration-700 hover:text-H' />
           <DiCss3 className='text-text  h-14 w-14 hover:duration-700 hover:text-C' />
@@ -26,15 +26,15 @@ function Skills () {
           <SiSequelize className='text-text h-12 w-12 mt-2 hover:duration-700 hover:text-S' />
         </div>
 
-        <h1 className='flex text-white text-2xl justify-start mt-5 ml-48 mr-96 border-b '>Data Base</h1>
-        <div className='flex justify-start mb-4  ml-48 mr-96 gap-6 py-8 px-5 border-t-8 border-mainDark'>
+        <h1 className='flex text-white text-2xl justify-start mt-5 ml-44 mr-96 border-b '>Data Base</h1>
+        <div className='flex justify-start mb-4 ml-44 mr-96 gap-6 py-8 px-5 border-t-8 border-mainDark'>
 
           <DiPostgresql className='text-text h-16 w-16 hover:duration-700 hover:text-P' />
           <SiMysql className='text-text h-16 w-16 hover:duration-700 hover:text-MY' />
         </div>
 
         <h1 className='flex text-white text-2xl justify-end mt-5 ml-96 mr-44 border-b '>Extras</h1>
-        <div className='flex justify-end mb-28 ml-96 mr-44 gap-6 py-8 px-5 border-t-8 border-mainDark'>
+        <div className='flex justify-end mb-28 ml-96 mr-44 gap-5 py-8 px-5 border-t-8 border-mainDark'>
 
           <DiGit className='text-text h-16 w-16 hover:duration-700 hover:text-G' />
           <DiGithubBadge className='text-text h-16 w-16 hover:duration-700 hover:text-GH' />
