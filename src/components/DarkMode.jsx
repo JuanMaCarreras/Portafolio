7cf476870436fc34256cc5a81b786439f24e706b
JuanMaCarrearas <manuucarreras@gmail.com>
2023-08-04 23:44:09 -0300

    dark mode done

---
+++ src/components/DarkMode.jsx
@@ -0,0 +1,30 @@
+import { BsMoonStarsFill, BsFillSunFill } from 'react-icons/bs'
+import { useState } from 'react'
+
+function DarkMode () {
+  const [isDarkMode, setIsDarkMode] = useState(false)
+
+  const changeDarkMode = () => {
+    setIsDarkMode(prevMode => !prevMode)
+    document.documentElement.classList.toggle('dark')
+  }
+  return (
+    <>
+      <button onClick={changeDarkMode}>
+        {
+         isDarkMode
+           ? (
+             <BsFillSunFill className='w-5 h-5 mr-3' />
+             )
+           : (
+             <BsMoonStarsFill className='w-5 h-5 mr-3' />
+             )
+        }
+
+      </button>
+
+    </>
+  )
+}
+
+export default DarkMode
