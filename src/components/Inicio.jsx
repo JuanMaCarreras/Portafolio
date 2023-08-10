327e90b3d3bc9a5dcd039b8d372cf95dbd4c2e12
JuanMaCarrearas <manuucarreras@gmail.com>
2023-08-10 17:53:51 -0300

    responsive mode

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,19 +1,20 @@
 function Inicio () {
   return (
-    <>
-      <main className='pl-36 pt-32 mb-44'>
-        <h1 className='text-6xl font-bold text-white'>Manuel Carreras</h1>
-        <h2 className='text-2xl font-semibold text-second pt-2 pl-2'>Full Stack Developer</h2>
+    <main
+      className='flex flex-col  ml-36 mt-32 mb-44 movil:mx-10 movil:mt-24 movil:items-center movil:justify-center animate-fade-in animate-duration-[2000ms]'
 
-        <p className='py-6 whitespace-pre-wrap mr-96 text-base text-text pl-2'>¡Hola! Soy un entusiasta del mundo digital fascinado por la magia que se encuentra detrás de la programación Full Stack.
-          Me encanta fusionar arte y funcionalidad para dar vida a proyectos únicos.
-          Estoy en la busca de mi primera experiencia laboral, estoy ansioso por unirme a un equipo creativo y dejar una huella digital en el mundo.
-          Siempre estoy abierto a nuevos desafíos y dispuesto a aprender. <br />
-          <br />
-          ¡Estoy emocionado por formar parte de un equipo increíble y contribuir con mi pasión por la programación!
-        </p>
-      </main>
-    </>
+    >
+      <h1 className='text-6xl font-bold text-text movil:text-4xl dark:text-white font-sans'>Manuel Carreras</h1>
+      <h2 className='font-mono text-title text-2xl font-semibold pt-2 pl-2 movil:pl-1 movil:text-2xl dark:text-second'>Full Stack Developer</h2>
+
+      <p className=' py-6 text-text mr-96 movil:pt-10 movil:mr-5 movil:text-sm movil:text-center movil:whitespace-pre-line movil:break-words text-base dark:text-textDark pl-2'>¡Hola! Soy un entusiasta del mundo digital fascinado por la magia que se encuentra detrás de la programación Full Stack.
+        Me encanta fusionar arte y funcionalidad para dar vida a proyectos únicos.
+        Estoy en la busca de mi primera experiencia laboral, estoy ansioso por unirme a un equipo creativo y dejar una huella digital en el mundo.
+        Siempre estoy abierto a nuevos desafíos y dispuesto a aprender. <br />
+        <br />
+        ¡Estoy emocionado por formar parte de un equipo increíble y contribuir con mi pasión por la programación!
+      </p>
+    </main>
   )
 }
 
