e4ba3030deb80b81e9cecbdd90271b0c66fd60f2
JuanMaCarrearas <manuucarreras@gmail.com>
2023-08-11 20:03:16 -0300

    App

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,15 @@ import './App.css'
 
 function App () {
   return (
-    <>
-      <div className=' bg-bg dark:bg-dark'>
-        <Nav />
-        <div className='pt-16'>
-          <Inicio />
-          <Proyectos />
-          <Skills />
-        </div>
+    <div className=' bg-bg dark:bg-dark'>
+      <Nav />
+      <div className='pt-16'>
+        <Inicio />
+        <Proyectos />
+        <Skills />
       </div>
-    </>
+    </div>
+
   )
 }
 
