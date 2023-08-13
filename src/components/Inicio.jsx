4ee51d5826eb137d882963fb5a1f0281cad20ecf
JuanMaCarrearas <manuucarreras@gmail.com>
2023-08-12 23:57:47 -0300

    responsive

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -7,7 +7,7 @@ function Inicio () {
       <h1 className='text-6xl font-bold text-text movil:text-4xl dark:text-white font-sans'>Manuel Carreras</h1>
       <h2 className='font-mono text-title text-2xl font-semibold pt-2 pl-2 movil:pl-1 movil:text-2xl dark:text-second'>Full Stack Developer</h2>
 
-      <p className=' py-6 text-text mr-96 movil:pt-10 movil:mr-5 movil:text-sm movil:text-center movil:whitespace-pre-line movil:break-words text-base dark:text-textDark pl-2'>¡Hola! Soy un entusiasta del mundo digital fascinado por la magia que se encuentra detrás de la programación Full Stack.
+      <p className=' py-6 text-text mr-96 movil:pt-10 movil:mr-3 movil:text-sm movil:text-center movil:whitespace-pre-line movil:break-words text-base dark:text-textDark pl-2'>¡Hola! Soy un entusiasta del mundo digital fascinado por la magia que se encuentra detrás de la programación Full Stack.
         Me encanta fusionar arte y funcionalidad para dar vida a proyectos únicos.
         Estoy en la busca de mi primera experiencia laboral, estoy ansioso por unirme a un equipo creativo y dejar una huella digital en el mundo.
         Siempre estoy abierto a nuevos desafíos y dispuesto a aprender. <br />
