9a51fdc3431e26634954a46a18fdeb2450df6ccf
JuanMaCarrearas <manuucarreras@gmail.com>
2023-08-22 21:16:56 -0300

    responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from 'react'
 import { FiLinkedin, FiGithub } from 'react-icons/fi'
+import DarkMode from './DarkMode'
+import EasterEgg from './EasterEgg'
 
 function Nav () {
   const [showName, setShowName] = useState()
 
   useEffect(() => {
     const handlerScroll = () => {
-      window.scrollY > 100 ? setShowName(true) : setShowName(false)
+      window.innerWidth < 713 ? setShowName(false) : setShowName(window.scrollY > 100)
     }
 
     window.addEventListener('scroll', handlerScroll)
@@ -17,60 +19,78 @@ function Nav () {
 
   return (
     <>
-      <header className={`fixed top-0 left-0 w-full py-6 shadow-md backdrop-blur-lg backdrop-filter bg-mainDark bg-opacity-90 ${showName ? 'show name' : ''}`}>
-        <nav className=' text-white '>
+      <header className={`fixed top-0 left-0 w-full py-6   shadow-md backdrop-blur-lg  bg-mainLight dark:bg-mainDark bg-opacity-90 ${showName ? 'show name' : ''}`}>
+        <nav className=' dark:text-white '>
+          {
+            showName &&
+              <div className=' flex items-center mt-1 absolute left-9 phone:left-0 phone:mt-2 phone:pt-0 tablet:mt-1 tablet:left-3 animate-fade-in'>
+                <p className='font-semibold phone:text-base dark:text-white text-2xl ml-0'> Manuel Carreras </p>
+              </div>
+          }
 
-          <ul className='flex justify-end gap-11 py-2 pr-16 text-lg'>
+          <ul className='flex justify-end gap-5 pb-2 pr-11 phone:gap-1 phone:pr-2 tablet:pr-2 animate-fade-down animate-duration-[1000ms]'>
 
-            {showName && <p className=' flex items-center font-semibold text-white text-2xl absolute left-12  animate-fade-in'>Manuel Carreras </p>}
-
-            <li
-              className='text-base
-              cursor-pointer
-              border-b-1 border-transparent
-              hover:duration-1000 hover:border-b-2 hover:border-second'
-            ><a href='#root'> Inicio </a>
+            <li className='pt-1 phone:pt-2'>
+              <a
+                href='#root'
+                className=' text-lg phone:text-sm font-semibold cursor-pointer hover:duration-700 hover:border-b hover:text-title dark:hover:text-second rounded-lg hover:bg-shadow2 dark:hover:bg-shadow3 dark:border-shadowB px-2 py-1'
+              > Inicio
+              </a>
 
             </li>
 
-            <li className='text-base
-              cursor-pointer
-              border-b-1 border-transparent
-              hover:duration-1000 hover:border-b-2 hover:border-second'
-            > <a href='#proyectos'> Proyectos </a>
+            <li className='pt-1 phone:pt-2'>
+              <a
+                href='#proyectos'
+                className='text-lg phone:text-sm font-semibold cursor-pointer  hover:duration-700 hover:border-b hover:text-title dark:hover:text-second rounded-lg hover:bg-shadow2 dark:hover:bg-shadow3 dark:border-shadowB px-2 py-1'
+              > Proyectos
+              </a>
             </li>
 
-            <li className=' text-base
-              cursor-pointer
-              border-b-1 border-transparent
-              hover:duration-1000 hover:border-b-2 hover:border-second'
-            > <a href='#skills'>Habilidades </a>
+            <li className='pt-1  phone:pt-2'>
+              <a
+                href='#skills'
+                className='text-lg phone:text-sm font-semibold cursor-pointer hover:duration-700 hover:border-b hover:text-title dark:hover:text-second rounded-lg hover:bg-shadow2 dark:hover:bg-shadow3 dark:border-shadowB px-2 py-1'
+              > Habilidades
+              </a>
+
             </li>
 
-            <li>
+            <li className='rounded-lg hover:bg-shadow2 dark:hover:bg-shadow3 px-2 py-1 hover:duration-700'>
               <a
                 href='https://github.com/JuanMaCarreras'
                 target='_blank'
                 rel='noreferrer'
+                className=''
               >
                 <FiGithub
-                  className='text-white w-6 h-6 hover:text-second hover:duration-700 '
+                  className='dark:text-white w-6 h-6 mt-1 hover:text-title dark:hover:text-second hover:duration-700 '
                 />
 
               </a>
             </li>
 
-            <li>
+            <li className='rounded-lg hover:bg-shadow2 dark:hover:bg-shadow3 px-2 py-1 hover:duration-1000'>
               <a
                 href='https://www.linkedin.com/in/manuel-carreras/'
                 target='_blank'
                 rel='noreferrer'
               >
                 <FiLinkedin
-                  className='text-white w-6 h-6 hover:text-second hover:duration-700 '
+                  className='dark:text-white w-6 h-6 mt-1 hover:text-title dark:hover:text-second hover:duration-700 '
                 />
 
               </a>
+            </li>
+
+            <li className='rounded-lg  hover:bg-shadow2 dark:hover:bg-shadow3 px-3 py-1 duration-1000'>
+
+              <DarkMode />
+
+            </li>
+
+            <li className='text-transparent'>
+              <EasterEgg />
             </li>
 
           </ul>
