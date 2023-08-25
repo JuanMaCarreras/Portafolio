6055d6fb77008114d27f2b388035124409fdea2c
JuanMaCarrearas <manuucarreras@gmail.com>
2023-08-25 18:12:00 -0300

    add cv page

--- src/components/Pdf.jsx
+++ src/components/Pdf.jsx
@@ -3,11 +3,13 @@ import cvPath from '../assets/Carreras_Manuel_CV.pdf'
 function Pdf () {
   return (
     <div className='absolute w-full h-full'>
+
       <object
         data={cvPath}
         type='application/pdf'
         width='100%' height='100%'
       />
+
     </div>
   )
 }
